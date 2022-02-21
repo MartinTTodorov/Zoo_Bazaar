4405ad10c9716701f33db74efb424c3badbf8c89
Stoil Yonchev <I478992@fhict.nl>
2022-02-21 15:48:20 +0100

    Start of the project

---
+++ ZooBazzar_Group03/ZooBazzar_Group03/Employeee/Caretaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooBazzar_Group03
+{
+    internal class Caretaker
+    {
+    }
+}
