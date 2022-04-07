1b58b335cece357d27ea4fd0072c89f4b2915dad
i463263 <i463263@fhict.nl>
2022-04-08 00:52:50 +0200

    #2

---
+++ ZooBazzar_03/Entities/EmployeeContract.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class EmployeeContract
+    {
+        private int id;
+        private Employee employee;
+        private DateTime startDate;
+        private DateTime endDate;
+        private double fte;
+        public string reason;
+        private bool isValid;
+
+        public EmployeeContract(int id, Employee employee, DateTime startDate, DateTime endDate, double fte, string reason, bool isValid)
+        {
+            this.id = id;
+            this.employee = employee;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.fte = fte;
+            this.reason = reason;
+            this.isValid = isValid;
+        }
+
+
+        public EmployeeContract()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public double Fte { get; set; }
+        public string Reason { get; set; }
+        public bool IsValid { get { return isValid; } }
+    }
+}
---
+++ ZooBazzar_03/LogicLayer/ContractManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace LogicLayer
+{
+    public class ContractManager
+    {
+        private List<EmployeeContract> contracts;
+        public ContractManager()
+        {
+            contracts = new List<EmployeeContract>();
+        }
+
+
+    }
+}
