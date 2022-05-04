83caf4cfb65d075a2cbf148c7c5890c549d56656
Stoil Yonchev <I478992@fhict.nl>
2022-05-04 16:30:56 +0200

    Login page

---
+++ ZooBazzar_03/ZooBazzar_Group03/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer;
+using LogicLayer;
+
+
+namespace ZooBazzar_Group03
+{
+    public static class DataManager
+    {
+        private static AccountManager accountManager = new AccountManager();
+        private static EmployeeManagment employeeManagment = new EmployeeManagment();
+        private static ContractManager contractManager = new ContractManager();
+        private static AnimalManager animalManager = new AnimalManager();
+        private static CageManager cageManager = new CageManager();
+        private static ScheduleManager scheduleManager = new ScheduleManager()
+    }
+}
