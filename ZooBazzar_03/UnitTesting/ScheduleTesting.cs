5262cc8d380059e913a78912c9dc0be4aae6441c
Radolina Petrova <478897@student.fontys.nl>
2022-05-11 12:49:16 +0200

    Unit tests schedule

---
+++ ZooBazzar_03/UnitTesting/ScheduleTesting.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer;
+using Entities;
+using LogicLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class ScheduleTesting
+    {
+
+
+        [TestMethod]
+        public void AddAccounts()
+        {
+            
+        }
+    }
+}
