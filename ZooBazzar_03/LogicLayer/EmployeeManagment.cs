ea789545ab60ae0b9d70370572f7b0bf6d5f38fa
i463263 <i463263@fhict.nl>
2022-05-28 09:07:13 +0200

    #8

--- ZooBazzar_03/LogicLayer/EmployeeManagment.cs
+++ ZooBazzar_03/LogicLayer/EmployeeManagment.cs
@@ -112,6 +112,11 @@ namespace LogicLayer
             return employees.Find(x => x.Name == EmployeeName);
         }
 
+        public Employee GetEmployeeById(int id)
+        {
+            return employees.Find(x => x.Id == id);
+        }
+
         public Employee GetEmployeeByUsername(string username)
         {
             return employees.Find(x => x.Account.Username == username);
