23c3c44db8eef87e7166bc3e4d2b47f0f11fa46a
Stoil Yonchev <I478992@fhict.nl>
2022-05-31 13:42:16 +0200

    Kick - off #13 & #14

--- ZooBazzar_03/Entities/Ticket.cs
+++ ZooBazzar_03/Entities/Ticket.cs
@@ -8,13 +8,68 @@ namespace Entities
 {
     public class Ticket
     {
-        //fields
+        //Fields
         private int id;
         private Customer customer;
         private TypeOfTicket typeOfTicket;
         private DateTime? date;
         private DateTime dateOfPurchase;
+        private bool isUsed;
 
-        //
+        public int Id { get { return id; } }
+        public Customer Customer
+        {
+            get { return customer; }
+            private set
+            {
+                if (value != null)
+                {
+                    customer = value;
+                }
+            }
+        }
+        public TypeOfTicket TypeOfTicket
+        {
+            get { return typeOfTicket; }
+            private set
+            {
+                if (Enum.IsDefined(typeof(TypeOfTicket), value))
+                {
+                    typeOfTicket = value;
+                }
+            }
+        }
+        public DateTime? Date { get { return date; } private set
+            {
+                if(value >= DateTime.UtcNow)
+                {
+                    date = value;
+                }
+            }
+        }
+
+        //Constructor
+        public Ticket(int id, Customer customer, TypeOfTicket typeOfTicket, DateTime date, DateTime dateOfPurchase)
+        {
+            this.id = id;
+            this.customer = customer;
+            this.typeOfTicket = typeOfTicket;
+            this.date = date;
+            this.dateOfPurchase = dateOfPurchase;
+            this.isUsed = false;
+            this.dateOfPurchase = DateTime.UtcNow;
+        }
+
+        //Method
+        public override string ToString()
+        {
+            return $"{customer}: {date} : {isUsed}";
+        }
+
+        public void UseTicket()
+        {
+            this.isUsed = true;
+        }
+
     }
 }
---
+++ ZooBazzar_03/LogicLayer/TicketManager.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class TicketManager
+    {
+        private List<Ticket> tickets;
+        public IList<Ticket> Tickets { get { return tickets.AsReadOnly(); } }
+
+        public TicketManager()
+        {
+            tickets = new List<Ticket>();
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            tickets.Add(ticket);
+        }
+
+        public void UpdateTicket(int id)
+        {
+            tickets.Find(t => t.Id == id).UseTicket();
+        }
+    }
+}
