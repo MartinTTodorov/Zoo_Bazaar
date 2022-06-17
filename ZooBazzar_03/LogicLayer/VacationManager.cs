911bef2611c9392fa36e1ac01dde1292bc9abd17
Martin Todorov <i481796@fhict.nl>
2022-06-17 09:33:36 +0200

    Ready for merge

---
+++ ZooBazzar_03/LogicLayer/VacationManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace LogicLayer
+{
+    public class VacationManager
+    {
+        public void RequestVacation(Employee employee)
+        {
+            //employee contract validation awaiting georgi
+            //schedule validation
+        }
+    }
+}
